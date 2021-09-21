99613b65e1b205a87dae7e63cb1eb99c2849b80a
Talapala Sneha <63858047+Talapala-Sneha@users.noreply.github.com>
2021-09-21 13:43:40 +0530

    Uploaded

---
+++ Account_Management/src/test/java/com/accountmanagement/Account_Management/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountmanagement.Account_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
